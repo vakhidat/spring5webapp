6088da4bdf7b6f620a45be8cb1036a0444741959
Vakhidat_Doskozhina <Vakhidat_Doskozhina@epam.com>
2020-01-17 17:10:05 +0600

    Spring MVC basics task finished

---
+++ src/main/java/com/vakhidat/spring5webapp/controller/AuthorController.java
@@ -0,0 +1,22 @@
+package com.vakhidat.spring5webapp.controller;
+
+import com.vakhidat.spring5webapp.repository.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+
+}
--- src/main/java/com/vakhidat/spring5webapp/model/Book.java
+++ src/main/java/com/vakhidat/spring5webapp/model/Book.java
@@ -16,7 +16,7 @@ public class Book {
     private Publisher publisher;
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
-    inverseJoinColumns = @JoinColumn(name = "author_id"))
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
     public Book() {
